44bff9ab432b01ea4f2f5fb4b8867a26c505dd1f
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-05-08 14:45:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,13 @@
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Zvishavane Rides</title>
-</head>
-<body>
-  <h1>Welcome to Zvishavane Rides!</h1>
-  <div>
-    <input type="text" id="pickup" placeholder="Pickup Location" />
-    <input type="text" id="dropoff" placeholder="Dropoff Location" />
-    <input type="number" id="price" placeholder="Offer Price" />
-    <button id="request-ride">Request Ride</button>
-  </div>
-</body>
-</html>
+export default function Home() {
+  return (
+    <div>
+      <h1>Welcome to Zvishavane Rides!</h1>
+      <div>
+        <input type="text" id="pickup" placeholder="Pickup Location" />
+        <input type="text" id="dropoff" placeholder="Dropoff Location" />
+        <input type="number" id="price" placeholder="Offer Price" />
+        <button id="request-ride">Request Ride</button>
+      </div>
+    </div>
+  );
+}
