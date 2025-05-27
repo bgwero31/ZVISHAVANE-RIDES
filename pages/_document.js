091f38af51e87d147bf043db98187d4ed647edf0
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-05-27 18:51:15 +0200

    Create _document.js

---
+++ pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document';
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        {/* Tailwind CSS CDN */}
+        <link
+          href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <body className="">
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
