dc35db581b8bff52c134f05717b3a4dc8cce0c2c
FLIGHTZONE SIGNALS <bgwero31@gmail.com>
2025-05-28 10:36:42 +0200

    Update _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -2,17 +2,22 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
   render() {
     return (
       <Html lang="en">
         <Head>
-          {/* Tailwind CSS CDN */}
+          {/* Tailwind CSS via CDN */}
           <link
             href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css"
             rel="stylesheet"
           />
         </Head>
-        <body>
+        <body className="bg-white dark:bg-gray-900 text-black dark:text-white">
           <Main />
           <NextScript />
         </body>
